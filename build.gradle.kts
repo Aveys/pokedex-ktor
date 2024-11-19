31c31969804a148c26079fc8bb22fc3535f68804
Arthur Veys <veysarthur@gmail.com>
2024-11-19 09:41:42 -0500

    Ktor 3 migration (#1)
    * Update build.gradle.kts for ktor 3
    
    * Update gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ val kotlinVersion: String by project
 val logbackVersion: String by project
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("io.ktor.plugin") version "2.3.11"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+    kotlin("jvm") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.1"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 group = "com.aveys"
@@ -17,6 +17,11 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    docker {
+        imageTag = "pokedex-ktor"
+    }
+}
 repositories {
     mavenCentral()
 }
@@ -28,7 +33,7 @@ dependencies {
     implementation("io.ktor:ktor-server-host-common-jvm")
     implementation("io.ktor:ktor-server-sessions")
     implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-call-logging")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-cio-jvm")
@@ -37,15 +42,14 @@ dependencies {
     implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-client-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.insert-koin:koin-ktor:3.5.6")
+    implementation("io.insert-koin:koin-ktor:4.0.1-Beta1")
     implementation("io.insert-koin:koin-logger-slf4j:3.5.6")
-    implementation("org.jetbrains.exposed:exposed-core:0.51.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.51.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.51.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.51.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.56.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.56.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.56.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.56.0")
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.11")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
 }
